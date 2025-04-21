81c5e4e6db6fcff0e6bf45e1123383f04a7b7660
Yasin Elbüz <yasinelbuz@gmail.com>
2025-04-22 02:59:38 +0300

    New login page and banned access page added. Also, empty post page created and unnecessary redirect file removed.

---
+++ src/pages/forbidden.tsx
@@ -0,0 +1,8 @@
+export default function Forbidden() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-2xl font-bold'>Forbidden</h1>
+        <p className='text-gray-500'>You are not authorized to access this page</p>
+    </div>
+  )
+}
---
+++ src/pages/login.tsx
@@ -0,0 +1,34 @@
+import { useNav } from "../hooks/use-nav"
+import { useLogin, useUser } from "../services/login"
+import { useNavigate } from "react-router-dom"
+
+export default function LoginPage() {
+    const { login } = useLogin()
+    const user = useUser()
+    const navigate = useNavigate()
+    const nav = useNav();
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <button
+        onClick={async () => {
+            try {
+                if (user?.name) {
+                    nav.home.go(navigate)();
+                    return;
+                }
+                
+                await login();
+                nav.home.go(navigate)();
+            } catch (error) {
+                console.error('Giriş işlemi başarısız:', error);
+            }
+        }}
+      className="cursor-pointer px-8 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition-colors duration-200 font-medium"
+    >
+      Giriş Yap
+    </button>
+  </div>
+  )
+}
