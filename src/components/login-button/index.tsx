f68666d566ce211c0686f7b864237ade0e6d1198
Yasin Elbüz <yasinelbuz@gmail.com>
2025-04-22 05:14:42 +0300

    Updated the "Log Out" text in the Login button to "Logout". Also, removed unnecessary spaces.

--- src/components/login-button/index.tsx
+++ src/components/login-button/index.tsx
@@ -7,6 +7,8 @@ export default function LoginButton() {
     const user = useUser();
     const { logout } = useLogout();
     const navigate = useNavigate()
+
+    
     if (user) {
       return (
         <div className="flex items-center gap-4">
@@ -21,7 +23,7 @@ export default function LoginButton() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M3 3a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3zm11.707 4.707a1 1 0 0 0-1.414-1.414L10 9.586 6.707 6.293a1 1 0 0 0-1.414 1.414L8.586 11l-3.293 3.293a1 1 0 1 0 1.414 1.414L10 12.414l3.293 3.293a1 1 0 0 0 1.414-1.414L11.414 11l3.293-3.293z" clipRule="evenodd" />
             </svg>
-            Çıkış Yap
+            Logout
           </button>
         </div>
       );
