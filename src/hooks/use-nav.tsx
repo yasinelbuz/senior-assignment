fbf732e22c0da949e9dfc0a68774f9a77ab381f8
Yasin Elbüz <yasinelbuz@gmail.com>
2025-04-22 05:10:04 +0300

    Added new route configurations; updated permissions for post, post edit, post comments and post creation pages with PERMISSIONS constants. Updated access permission control.

--- src/hooks/use-nav.tsx
+++ src/hooks/use-nav.tsx
@@ -3,6 +3,7 @@
 import { generatePath, useNavigate } from 'react-router-dom'
 
 import { USER } from '../config/user'
+import { PERMISSIONS } from '../config/permission';
 
 export type RouteParam<T extends string> = T extends `:${infer P}` ? P : never
 
@@ -11,15 +12,40 @@ const routes = [
     {
       name: 'home',
       path: '/',
-      permissions: ['VIEW_COMMENTS', 'CREATE_POST']
+      permissions: [PERMISSIONS.VIEW_COMMENTS, PERMISSIONS.CREATE_POST]
+    },
+    {
+      name: 'posts',
+      path: '/posts',
+      permissions: [PERMISSIONS.VIEW_POSTS]
+    },
+    {
+      name: 'post',
+      path: '/posts/:id',
+      permissions: [PERMISSIONS.VIEW_POSTS],
+    },
+    {
+      name: 'post-edit',
+      path: '/posts/:id/edit',
+      permissions: [PERMISSIONS.EDIT_POST]
+    },
+    {
+      name: 'post-comments',
+      path: '/posts/:id/comments', 
+      permissions: [PERMISSIONS.VIEW_COMMENTS]
+    },
+    {
+      name: 'post-create',
+      path: 'create',
+      permissions: [PERMISSIONS.CREATE_POST]
     },
     {
       name: 'login',
-      path: '/login', 
+      path: '/login',
       permissions: []
     },
     {
-      name: 'forbidden',
+      name: 'forbidden', 
       path: '/403',
       permissions: []
     }
@@ -33,7 +59,7 @@ export const useNav = () => {
         const hasPermission = !route.permissions || route.permissions.every((p) => USER.permissions.includes(p));
 
         if (!hasPermission) {
-          alert('Bu sayfaya erişim yetkiniz yok.');
+          alert('You don\'t have permission to access this page.');
           navigate('/403');
           return;
         }
